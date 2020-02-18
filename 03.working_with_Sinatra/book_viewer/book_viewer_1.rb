b78d96c4ab9f24ad4b81f976a782fd350ff8cf4a
Wendy <wendytsai1988@gmail.com>
2020-02-18 06:24:37 +0100

    files for creating book viewer application

---
+++ 03.working_with_Sinatra/book_viewer/book_viewer_1.rb
@@ -0,0 +1,25 @@
+require "tilt/erubis"
+require "sinatra"
+require "sinatra/reloader"
+
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb(:home)
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+  @title = "Chapter #{number}: #{chapter_name}"
+
+  @chapter = File.readlines("data/chp#{number}.txt", sep = "\n\n")
+  
+  erb(:chapter)
+end
+
+
---
+++ 03.working_with_Sinatra/book_viewer/example.rb
@@ -0,0 +1,6 @@
+text = "this is a good day.
+I wish to go out and enjoy the sun.
+
+but this is goign to be a bit challenge."
+
+p text.split("\n\n")
